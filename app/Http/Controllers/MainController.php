aea0a4f05016eabc73b97280676321e9aaaef5eb
tonnikrava <tonnikrava@gmail.com>
2019-04-29 11:01:38 +0300

    Первая копия резервная

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -12,7 +12,7 @@ use App\Tovari;
 
 class MainController extends Controller
 {
-    public function getIndex() //Вывод главной страницы приложения2
+    public function getIndex() //Вывод главной страницы 2
     {
         $material = Page::where('slug', '=', 'index')->get();
         $novosti = Nowosti::latest()->limit(4)->get();
